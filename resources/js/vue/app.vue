5ddda060e94eb1efe1db3734e919ba30aa82cd61
chuongvu2001 <chuongvu2806@gmail.com>
2021-08-29 15:54:01 +0700

    vuejs todolist

---
+++ resources/js/vue/app.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+        <div class="todoListContianer">
+            <div class="heading">
+                <h2 id="title">Todo List</h2>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+
+import addItem from './addItem.vue'
+import listView from "./listView.vue"
+
+export default {
+    components:{
+        addItem,
+        listView
+    }
+}
+
+</script>
+
+
+<style scoped>
+    .todoListContianer {
+        width: 350px;
+        margin: auto;
+    }
+    .heading{
+        background: #e6e6e6;
+        padding: 10px;
+    }
+    #title {
+        text-align: center;
+
+    }
+
+</style>
---
+++ resources/js/vue/listView.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        List View
+    </div>
+</template>
+
+<script>
+export default {
+    name: "listView.vue"
+}
+</script>
+
+<style scoped>
+
+</style>
